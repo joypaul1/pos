327ed544b26cffe89888e3ac0d2e359448bb540a
joypaul1 <jyla5102555@gmail.com>
2022-04-21 15:53:42 +0600

    dd

--- resources/views/backend/admin/invoice/pdf/bank_pdf.blade.php
+++ resources/views/backend/admin/invoice/pdf/bank_pdf.blade.php
@@ -4,10 +4,9 @@
 		<meta charset="UTF-8" />
 		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-		<link
-			href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
-			rel="stylesheet"
-		/>
+
+		<link rel="stylesheet" href="{{ asset('font-awesome.min.css') }}">
+		<link rel="stylesheet" href="{{ asset('bootstrap.min.css') }}">
 
 		<style>
 			* {
@@ -181,12 +180,7 @@
 			}
 		</style>
 
-		<link
-			href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
-			rel="stylesheet"
-			integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
-			crossorigin="anonymous"
-		/>
+
 
 		<title>{{$owner->name}}</title>
 	</head>
@@ -461,21 +455,10 @@
 			</div>
 		</div>
 
-		<script
-			src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-			integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-			crossorigin="anonymous"
-		></script>
-		<script
-			src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
-			integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
-			crossorigin="anonymous"
-		></script>
-		<script
-			src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-			integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
-			crossorigin="anonymous"
-		></script>
+
+		<script src="{{ asset('jquery-3.3.1.slim.min.js') }}" ></script>
+		<script src="{{ asset('popper.min.js') }}" ></script>
+		<script src="{{ asset('bootstrap.min.js') }}" ></script>
 
 		<script>
 			$('#printInvoice').click(function () {
